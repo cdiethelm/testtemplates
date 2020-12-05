9d88055a9bc5e5148da0f1a2e0f86a079f1bd5fb
cdiethelm <c.diethelm@diethelm-aufzuege.ch>
2020-12-05 17:36:51 +0100

    f

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 func startHTTP() {
 	r := mux.NewRouter()
 
-	allowedHeaders := handlers.AllowedHeaders([]string{"Content-Type", "SecureKey"})
+	allowedHeaders := handlers.AllowedHeaders([]string{"Content-Type"})
 	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
 	allowedMethods := handlers.AllowedMethods([]string{"POST", "GET"})
 
@@ -36,12 +36,12 @@ func startHTTP() {
 	r.HandleFunc("/template/", templateHandler).Methods("GET")
 
 	http.Handle("/", r)
-
+	
 	log.Fatal(http.ListenAndServe(":5000", handlers.CORS(allowedHeaders, allowedOrigins, allowedMethods)(r)))
 }
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Chris is great</h1>")
+	fmt.Fprintf(w, "<h1>This is a test</h1>")
 }
 
 func templateHandler(w http.ResponseWriter, r *http.Request) {
